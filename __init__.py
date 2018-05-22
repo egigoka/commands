560e80afa438abe529ae2845ad1ed1dcd665f1b5
egigoka <egigoka@gmail.com>
2018-05-22 18:55:35 +0300

    "create cli8"

--- __init__.py
+++ __init__.py
@@ -781,50 +781,7 @@ try:
 
     if FRACKING_classes_speed_tweaking: LoadTimeBenchMark.end("class Int loaded in", quiet_if_zero=True, start_immideately=True)
 
-    class CLI():
-        @staticmethod
-        def get_y_n(question="", answer=None):
-            def check_answer(string):
-                if inputtt == "y":
-                    return True
-                if inputtt == "n":
-                    return False
-            if answer: check_answer(answer)
-            while True:
-                inputtt = input(str(question) + " (y/n)?")
-                inputtt = inputtt.strip(" ")
-                check_answer(inputtt)
 
-        wait_update_pos = 0
-
-        @classmethod
-        def wait_update(CLI, quiet=False):
-            if CLI.wait_update_pos == 0:
-                stick = "|"
-            elif CLI.wait_update_pos == 1:
-                stick = "/"
-            elif CLI.wait_update_pos == 2:
-                stick = "-"
-            elif CLI.wait_update_pos == 3:
-                stick = "\ "[:1]
-            elif CLI.wait_update_pos == 4:
-                stick = "|"
-            elif CLI.wait_update_pos == 5:
-                stick = "/"
-            elif CLI.wait_update_pos == 6:
-                stick = "-"
-            elif CLI.wait_update_pos == 7:
-                stick = "\ "[:1]
-                CLI.wait_update_pos = -1
-            CLI.wait_update_pos += 1
-            if not quiet:
-                Print.rewrite(stick)
-            else:
-                return stick
-
-        @staticmethod
-        def progressbar(count, of):
-            Console.width()
 
     if FRACKING_classes_speed_tweaking: LoadTimeBenchMark.end("class CLI loaded in", quiet_if_zero=True, start_immideately=True)
 
