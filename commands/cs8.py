dae119e7f4fbe486b81752f9efbaadefce91e64d
egigoka <egigoka@gmail.com>
2018-06-14 15:09:29 +0300

    "update dirify func to support wildcards"

--- commands/cs8.py
+++ commands/cs8.py
@@ -2,15 +2,17 @@
 # -*- coding: utf-8 -*-
 """Internal module with shit functions
 """
-__version__ = "2.1.0"
+__version__ = "2.2.0"
 
 
 def dirify(object_, regexp="*", quiet=False):
     """Print and return list of object public attributes
     :param object_: object to input
+    :param regexp: string with shell-like wildcards for filtering
     :param quiet: suppress output to console
     :return: list of object public attributes
     """
+    from .list8 import List
     output = []
     for subobj in List.wildcard_search(dir(object_), regexp):
         if "__" not in subobj:
