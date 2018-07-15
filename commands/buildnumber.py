36e1ba8f16fd480b176498fe1b35d6318c4db356
egigoka <egigoka@gmail.com>
2018-07-15 16:28:35 +0300

    bugfix build storage

--- commands/buildnumber.py
+++ commands/buildnumber.py
@@ -1,13 +1,3 @@
 #! python3
 # -*- coding: utf-8 -*-
-build = 367#! python3
-# -*- coding: utf-8 -*-
-build = 368#! python3
-# -*- coding: utf-8 -*-
-build = 369#! python3
-# -*- coding: utf-8 -*-
-build = 370#! python3
-# -*- coding: utf-8 -*-
-build = 371#! python3
-# -*- coding: utf-8 -*-
-build = 372
+build = 373
