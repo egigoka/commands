69170bca567fb5e4eefcce8a718e1451e9581ee5
egigoka <egigoka@gmail.com>
2018-08-02 20:40:02 +0300

    "added Random.string_unicode"

--- commands/random8.py
+++ commands/random8.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """Internal module with functions for creating some random values.
 """
-__version__ = "0.3.2"
+__version__ = "0.4.0"
 
 
 class Random:
@@ -40,6 +40,39 @@ class Random:
             string_of_symbols = string.ascii_uppercase + string.ascii_lowercase + string.digits
         import random
         return ''.join(random.choices(string_of_symbols, k=length))
+
+    @staticmethod
+    def string_unicode(length):
+        """
+        :param length: int, length of output string
+        :return: string, with random unicode symbols
+        """
+        import random
+
+        try:
+            get_char = unichr  # py2
+        except NameError:
+            get_char = chr
+
+        # Update this to include code point ranges to be sampled
+        include_ranges = [
+            (0x0021, 0x0021),
+            (0x0023, 0x0026),
+            (0x0028, 0x007E),
+            (0x00A1, 0x00AC),
+            (0x00AE, 0x00FF),
+            (0x0100, 0x017F),
+            (0x0180, 0x024F),
+            (0x2C60, 0x2C7F),
+            (0x16A0, 0x16F0),
+            (0x0370, 0x0377),
+            (0x037A, 0x037E),
+            (0x0384, 0x038A),
+            (0x038C, 0x038C),]
+
+        alphabet = [get_char(code_point) for current_range in include_ranges
+            for code_point in range(current_range[0], current_range[1] + 1)]
+        return ''.join(random.choice(alphabet) for i in range(length))
 
     @staticmethod
     def boolean():
