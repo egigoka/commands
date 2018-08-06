c5b2fdba5e8523e292df7bfc3c00d674e3a4e656
egigoka <egigoka@gmail.com>
2018-08-06 22:00:19 +0300

    "version updating improvement"

--- gi.py
+++ gi.py
@@ -11,16 +11,16 @@ from commands.print8 import Print
 # CHANGING VERSION
 version_prefix = '__version__ = "'
 version_suffix = '"'
-version_path = Path.extend(Path.working(), "commands", "_version.py")
-version_text = File.read(version_path)
-__version__ = Str.substring(version_text, version_prefix, version_suffix)
-alphanumber = Str.get_integers(__version__)[-1]
-old_version = version_prefix + __version__ + version_suffix
-new_version = version_prefix + __version__.replace("-alpha"+str(alphanumber),"-alpha"+str(alphanumber+1))+version_suffix
-new_version_text = version_text.replace(old_version, new_version)
-File.write(version_path, new_version_text, mode="w")
+version_path = Path.extend(Path.working(), "commands", "_version.py")  # path to version file
+version_text = File.read(version_path)  # text from version file
+__version__ = Str.substring(version_text, version_prefix, version_suffix)  # get version from plane text
+alphanumber = Str.get_integers(__version__)[-1]  # get only integer of alpha version
+old_version = version_prefix + __version__ + version_suffix  # old string, that currently in file
+new_version = version_prefix + __version__.replace("-alpha"+str(alphanumber),"-alpha"+str(alphanumber+1))+version_suffix  # new string to replace old version
+new_version_text = version_text.replace(old_version, new_version)  # replacing
+File.write(version_path, new_version_text, mode="w")  # write result from file
 
-Print.colored("uploadin",  new_version.lstrip(version_prefix).rstrip(version_suffix), "grey", "on_white")
+Print.colored("uploadin",  new_version.lstrip(version_prefix).rstrip(version_suffix), "grey", "on_white")  # print to notice difference
 # CHANGING VERSION END
 
 
