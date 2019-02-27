42350a615bbcebf42d8af7c586fd3411c852da2d
Egor Egorov <egigoka@gmail.com>
2019-02-27 19:00:36 +0300

    "added Base64.to_bytes, .from_bytes, .to_file, .from_file"

--- commands/base64_9.py
+++ commands/base64_9.py
@@ -2,24 +2,47 @@
 # -*- coding: utf-8 -*-
 """Internal module to work with base64
 """
-__version__ = "1.0.4"
+__version__ = "1.1.0"
 
 
 class Base64:  # pylint: disable=too-few-public-methods
     """Class to work with base64
     """
-    @staticmethod
-    def to_png(string, filename):
+    @classmethod
+    def to_png(cls, string, filename):
         """
         :param string: string base64
         :param filename: filename to save png
         :return: None
         """
-        import base64
-        png_recovered = base64.decodebytes(string.encode('ascii'))  # decode string to pure picture
+        
+        
         filename = str(filename)
         if not filename.endswith(".png"):
             filename = filename + ".png"
+        png_recovered = cls.to
         file = open(filename, "wb")
         file.write(png_recovered)
         file.close()
+    
+    @staticmethod
+    def to_bytes(string):
+        import base64
+        return base64.decodebytes(string.encode('ascii'))
+    
+    @staticmethod
+    def to_string(bytes_):
+        import base64
+        return base64.encodebytes(bytes_)
+    
+    @classmethod
+    def to_file(cls, string, filename):
+        from .file9 import File
+        file_bytes = cls.to_bytes(string)
+        File.write(filename, string, mode="wb")
+    
+    @classmethod
+    def from_file(cls, filename):
+        with open(filename, mode="rb") as file:
+            file_bytes = file.read()
+        return cls.to_string(file_bytes)
