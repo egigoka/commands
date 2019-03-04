782122f874f5be69cfd2cb62c2e9244de2473506
Egor Egorov <egigoka@gmail.com>
2019-03-04 23:10:13 +0300

    "debug"

--- commands/_version.py
+++ commands/_version.py
@@ -1,7 +1,7 @@
 #! python3
 # -*- coding: utf-8 -*-
 """Here version stay and thoughts for updates"""
-__version__ = "9.0.0-alpha210"
+__version__ = "9.0.0-alpha211"
 
 # TODO for 9.0.0 release:
 #    !done! OS class vars not strings, but booleans
