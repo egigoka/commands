cbd23b14cdb862f12e73b4a26f3f151aa467d2a3
Egor Egorov <egigoka@gmail.com>
2019-03-21 17:53:05 +0300

    "update doc"

--- commands/_version.py
+++ commands/_version.py
@@ -1,7 +1,7 @@
 #! python3
 # -*- coding: utf-8 -*-
 """Here version stay and thoughts for updates"""
-__version__ = "9.0.0-alpha247"
+__version__ = "9.0.0-alpha248"
 
 # TODO for 9.0.0 release:
 #    !done! OS class vars not strings, but booleans
