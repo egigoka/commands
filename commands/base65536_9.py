8c6c998fb207e5a562be331eace1f684c91b9b91
egigoka <egigoka@gmail.com>
2019-04-01 21:30:51 +0300

    "bugfix Base655363"

--- commands/base65536_9.py
+++ commands/base65536_9.py
@@ -1,7 +1,7 @@
 #! python3
 # -*- coding: utf-8 -*-
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 
 class Base65536:
@@ -11,6 +11,7 @@ class Base65536:
         bytes = input.encode()
         b65k = base65536.encode(bytes)
         return b65k
+
     @staticmethod
     def b65kdecode(input):
         import base65536
@@ -27,6 +28,6 @@ class Base65536:
     @classmethod
     def from_file(cls, file_path):
         from .file9 import File
-        encoded = File.read(file_path)
+        encoded = File.read(file_path, auto_detect_encoding=True)
         decoded = cls.b65kdecode(encoded)
         return decoded
