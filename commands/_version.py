4ff1a28281eb0a9b88f11ba27cdad8bf0760dcfd
egigoka <egigoka@gmail.com>
2019-04-01 21:58:34 +0300

    "remove Base65536"

--- commands/_version.py
+++ commands/_version.py
@@ -1,7 +1,7 @@
 #! python3
 # -*- coding: utf-8 -*-
 """Here version stay and thoughts for updates"""
-__version__ = "9.0.0-alpha270"
+__version__ = "9.0.0-alpha271"
 
 # TODO for 9.0.0 release:
 #    !done! OS class vars not strings, but booleans
