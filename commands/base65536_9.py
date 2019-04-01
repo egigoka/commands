b14e316d749838957c1d30ec4f35239b0c0cd1d2
egigoka <egigoka@gmail.com>
2019-04-01 20:36:59 +0300

    "updated Resources"

--- commands/base65536_9.py
+++ commands/base65536_9.py
@@ -5,12 +5,14 @@ __version__ = "0.0.2"
 
 
 class Base65536:
+    @staticmethod
     def to_file(string, file_path):
         import base65536
         from .file9 import File
         decoded = base65536.decode(string)
         File.write(file_path, decoded)
 
+    @staticmethod
     def from_file(file_path):
         import base65536
         from .file9 import File
