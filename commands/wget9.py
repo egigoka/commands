751b7f5bf2ade0c31ab0733e0012fa8f478d2986
Egor Egorov <egigoka@gmail.com>
2019-06-25 13:07:20 +0300

    "added wget executable for windows"

--- commands/wget9.py
+++ commands/wget9.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 """Internal module wrapper to cli wget
 """
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 
 class Wget:  # pylint: disable=too-few-public-methods
     """Class wrapper to cli wget
     """
-    @staticmethod
-    def download(url, output_filename, quiet=False, no_check_certificate=False):  # pylint: disable=inconsistent-return-statements
+    @classmethod
+    def download(cls, url, output_filename, quiet=False, no_check_certificate=False, wget_path="wget"):  # pylint: disable=inconsistent-return-statements
         """Wrapper to wget cli
         <br>`param url` string, url to some file
         <br>`param output_filename` string, path to filename
@@ -28,11 +28,15 @@ class Wget:  # pylint: disable=too-few-public-methods
         if no_check_certificate:
             no_check_certificate_cli_arg = "--no-check-certificate"
 
-        command = f"wget {no_check_certificate_cli_arg} {url} -O {output_filename} {arguments}"
+        command = f"{wget_path} {no_check_certificate_cli_arg} {url} -O {output_filename} {arguments}"
         if not Dir.exist(os.path.split(output_filename)[0]) and os.path.split(output_filename)[0]:
             Dir.create(os.path.split(output_filename)[0])
         try:
             return Console.get_output(command, print_std=not quiet)
         except FileNotFoundError as exception:
+            if wget_path == "wget":
+                from .path9 import Path
+                return cls.download(url=url, output_filename=output_filename, no_check_certificate=no_check_certificate,
+                                    wget_path=Path.combine(Path.commands(), "wget"))
             raise OSError(exception, "install wget")
         # Another way to fix blocks by creating ~/.wgetrc file https://stackoverflow.com/a/34166756
