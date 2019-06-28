4eb45859d2d3987095a8aa7778e0b73ef7e2dfb3
Egor Egorov <egigoka@gmail.com>
2019-06-28 16:26:13 +0300

    "cleanup"

--- commands/_version.py
+++ commands/_version.py
@@ -1,7 +1,7 @@
 #! python3
 # -*- coding: utf-8 -*-
 """Here version stay and thoughts for updates"""
-__version__ = "9.0.0-alpha380"
+__version__ = "9.0.0-alpha381"
 
 # TODO for 9.0.0 release:
 #    !done! OS class vars not strings, but booleans
