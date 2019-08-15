cf04f863c5b6b5ab1b343b83ef6dfbe25e2eab61
Egor Egorov <egigoka@gmail.com>
2019-08-15 13:51:44 +0300

    "bugfix and update dirify"

--- commands/funcs9.py
+++ commands/funcs9.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 """Internal module with shit functions
 """
-__version__ = "3.0.2"
+__version__ = "3.1.0"
 
 
-def dirify(object_, wildcard="*"):
+def dirify(_object, wildcard="*"):
     """Print and return list of object public attributes
     <br>`param object_` object to input
     <br>`param regexp` string with shell-like wildcards for filtering
@@ -15,13 +15,18 @@ def dirify(object_, wildcard="*"):
     from .list9 import List
     from .str9 import Str
     longest_name = 0
-    for attr_str in List.wildcard_search(dir(object_), wildcard):
+    for attr_str in List.wildcard_search(dir(_object), wildcard):
         if "__" not in attr_str:
             if len(attr_str) > longest_name:
                 longest_name = len(attr_str)
-    for attr_str in List.wildcard_search(dir(object_), wildcard):
+    for attr_str in List.wildcard_search(dir(_object), wildcard):
         if "__" not in attr_str:
-            print(Str.rightpad(attr_str, longest_name, " "), type(object_.__getattribute__(attr_str)))
+            try:
+                _type = type(_object.__getattribute__(attr_str))
+            except TypeError:
+                _type = type(_object.__getattribute__(_object, attr_str))
+            _type = Str.substring(_type, "<class ", ">", safe=True)
+            print(Str.rightpad(attr_str, longest_name, "—"), _type, sep=">")
 
 
 def multiple(*types):
