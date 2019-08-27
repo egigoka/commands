e40ec4f755e554d31d1ff50aa5d5d0eab70fbc31
Egor Egorov <egigoka@gmail.com>
2019-08-27 16:36:08 +0300

    "bugfix File.read"

--- commands/file9.py
+++ commands/file9.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """Internal module to work with files
 """
-__version__ = "1.0.6"
+__version__ = "1.0.7"
 # pylint: disable=c-extension-no-member
 
 
@@ -211,7 +211,7 @@ class File:
                     file.close()
                     return content
         elif mode in ["b", "rb"]:
-            with open(path, "rb", encoding=encoding) as file:
+            with open(path, "rb") as file:
                 content = file.read()
                 file.close()
                 return content
