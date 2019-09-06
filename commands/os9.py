9f5dbee109eb24949c322ee1dd4de667082aaa2c
Egor Egorov <egigoka@gmail.com>
2019-09-06 19:30:58 +0300

    "rewrited OS sub-module with properties for lazy module loading"

--- commands/os9.py
+++ commands/os9.py
@@ -2,91 +2,190 @@
 # -*- coding: utf-8 -*-
 """Internal module to check some environment properties
 """
-import sys  # static module
-import platform  # static module
-__version__ = "2.3.1"
+__version__ = "3.0.0"
 
 
 class OS:  # pylint: disable=too-few-public-methods
     """Class with some environment properties
     """
-    sys_version_info = sys.version_info
-    is_python3 = sys.version_info >= (3, 0)
-    python_version_major = sys.version_info.major # int of major python version
-    python_commandline_version = ""
-    if is_python3:
-        python_commandline_version = "3" # string of addable "3" to commandline apps if python is 3rd version
-
-    nt_family = False
-    unix_family = False
-
-    windows = False
-    macos = False
-    linux = False
-
-    windows_version = None  # only on Windows, integer of major version of Windows
-
-    display = None  # didn't work yet
-    cyrillic_support = None  # boolean variable of cyrrilic output support
-
-    sys_platfrom = sys.platform
-    if sys_platfrom == "linux" or sys_platfrom == "linux2":
-        linux = True
-        name = "linux"
-    elif sys_platfrom == "win32" or sys_platfrom == "cygwin":
-        windows = True
-        windows_version = sys.getwindowsversion().major
-        name = "windows"
-    elif sys_platfrom == "darwin":
-        macos = True
-        name = "macos"
-    else:
-        name = "unknown"
-
-    architecture = platform.architecture()[0]
-
-    if platform.python_implementation == "PyPy":
-        python_implementation = "pypy"
-    else:
-        python_implementation = "cpython"
-
-    if windows:
-        nt_family = True
-    elif macos or linux:
-        unix_family = True
-
-    try:
-        if linux:
-            from Xlib.display import Display
-        display = True
-    except ImportError:
-        display = False
-        print("Your system haven't display -_-")
-
-    @staticmethod
-    def running_in_repl():
+    @property
+    def is_python3(self):
         try:
-            sys.ps1  # pylint: disable = pointless-statement, no-member
-            sys.ps2  # pylint: disable = pointless-statement, no-member
-            return True
+            return self._is_python3
         except AttributeError:
-            return False
+            import sys
+            self._is_python3 = sys.version_info >= (3, 0)
+            return self._is_python3
 
-    try:
-        #if windows:
-        #cyr_line = "йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ"
-        cyr_line = "йЙ"
-        if windows and sys.version_info < (3, 6):
+    @property
+    def python_version_major(self):
+        try:
+            return self._python_version_major
+        except AttributeError:
+            import sys
+            self._python_version_major = sys.version_info.major
+            return self._python_version_major
+
+    @property
+    def python_commandline_version(self):
+        try:
+            return self._python_commandline_version
+        except AttributeError:
+            self._python_commandline_version =  ""
+            if self.is_python3:
+                self._python_commandline_version = "3"
+            return self._python_commandline_version
+
+    @property
+    def sys_platfrom(self):
+        try:
+            return self._sys_platfrom
+        except AttributeError:
+            import sys
+            self._sys_platfrom = sys.platform
+            return self._sys_platfrom
+
+    @property
+    def windows(self):
+        try:
+            return self._windows
+        except AttributeError:
+            self._windows = self.sys_platfrom == "win32" or self.sys_platfrom == "cygwin"
+            return self._windows
+
+    @property
+    def windows_version(self):
+        try:
+            return self._windows_version
+        except AttributeError:
+            import sys
+            self._windows_version = sys.getwindowsversion().major
+            return self._windows_version
+
+    @property
+    def linux(self):
+        try:
+            return self._linux
+        except AttributeError:
+            self._linux = self.sys_platfrom == "linux" or self.sys_platfrom == "linux2"
+            return self._linux
+
+    @property
+    def macos(self):
+        try:
+            return self._macos
+        except AttributeError:
+            self._macos = self.sys_platfrom == "darwin"
+            return self._macos
+
+    @property
+    def name(self):
+        try:
+            return self._name
+        except AttributeError:
+            if self.sys_platfrom == "linux" or self.sys_platfrom == "linux2":
+                self._name = "linux"
+            elif self.sys_platfrom == "win32" or self.sys_platfrom == "cygwin":
+                self._name = "windows"
+            elif self.sys_platfrom == "darwin":
+                self._name = "macos"
+            else:
+                self._name = "unknown"
+            return self._name
+
+    @property
+    def architecture(self):
+        try:
+            return self._architecture
+        except AttributeError:
+            import platform
+            self._architecture = platform.architecture()[0]
+            return self._architecture
+
+    @property
+    def python_implementation(self):
+        try:
+            return self._python_implementation
+        except AttributeError:
+            import platform
+            if platform.python_implementation == "PyPy":
+                self._python_implementation = "pypy"
+            else:
+                self._python_implementation = "cpython"
+            return self._python_implementation
+
+
+    @property
+    def nt_family(self):
+        try:
+            return self._nt_family
+        except AttributeError:
+            self._nt_family = self.windows
+            return self._nt_family
+
+
+    @property
+    def unix_family(self):
+        try:
+            return self._unix_family
+        except AttributeError:
+            self._unix_family = self.macos or self.linux
+            return self._unix_family
+
+
+    @property
+    def display(self):
+        try:
+            return self._display
+        except AttributeError:
             try:
-                import win_unicode_console  # pylint: disable=import-error
-                win_unicode_console.enable()
-            except:  # pylint: disable=bare-except
-                pass
-        for cyr_symbol in cyr_line:
-            print(cyr_symbol * 2 + "\r", end="")
-        print("  \r", end="")
-        cyrillic_support = True
-    except (UnicodeEncodeError, PermissionError) as err:
-        cyrillic_support = False
+                if self.linux:
+                    from Xlib.display import Display
+                self._display = True
+            except ImportError:
+                self._display = False
+            return self._display
 
-    
+
+    @property
+    def running_in_repl(self):
+        try:
+            return self._running_in_repl
+        except AttributeError:
+            try:
+                import sys
+                sys.ps1  # pylint: disable = pointless-statement, no-member
+                sys.ps2  # pylint: disable = pointless-statement, no-member
+                self._running_in_repl = True
+            except AttributeError:
+                self._running_in_repl = False
+            return self._running_in_repl
+
+
+    @property
+    def cyrillic_support(self):
+        try:
+            return self._cyrillic_support
+        except AttributeError:
+            self._cyrillic_support = False
+            try:
+                # if windows:
+                # cyr_line = "йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ"
+                cyr_line = "йЙ"
+                import sys
+                if self.windows and sys.version_info < (3, 6):
+                    try:
+                        import win_unicode_console  # pylint: disable=import-error
+                        win_unicode_console.enable()
+                    except:  # pylint: disable=bare-except
+                        pass
+                for cyr_symbol in cyr_line:
+                    print(cyr_symbol * 2 + "\r", end="")
+                print("  \r", end="")
+                self._cyrillic_support = True
+            except (UnicodeEncodeError, PermissionError) as err:
+                pass
+            return self._cyrillic_support
+
+
+OS = OS()
