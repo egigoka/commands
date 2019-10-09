dc18f1412eed7e677f69b664929e2fa5260a0b7a
Egor Egorov <egigoka@gmail.com>
2019-10-09 12:13:46 +0300

    "updated dirify"

--- commands/funcs9.py
+++ commands/funcs9.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """Internal module with shit functions
 """
-__version__ = "3.1.4"
+__version__ = "3.2.0"
 
 
 def dirify(_object, wildcard="*"):
@@ -15,22 +15,21 @@ def dirify(_object, wildcard="*"):
     from .list9 import List
     from .str9 import Str
     longest_name = 0
-    for attr_str in List.wildcard_search(dir(_object), wildcard):
-        if "__" not in attr_str:
-            if len(attr_str) > longest_name:
-                longest_name = len(attr_str)
-    for attr_str in List.wildcard_search(dir(_object), wildcard):
-        if "__" not in attr_str:
-            try:
-                _type = type(_object.__getattribute__(attr_str))
-            except TypeError:
-                _type = type(_object.__getattribute__(_object, attr_str))
-            except AttributeError:
-                _type = "AttributeError happened"
-            except OSError:
-                _type = "OSError happened"
-            _type = Str.substring(str(_type), "<class ", ">", safe=True)
-            print(Str.rightpad(attr_str, longest_name, "—"), _type, sep=">")
+    attrs = List.wildcard_search(dir(_object), wildcard)
+    for attr_str in attrs:
+        if len(attr_str) > longest_name:
+            longest_name = len(attr_str)
+    for attr_str in attrs:
+        try:
+            _type = type(_object.__getattribute__(attr_str))
+        except TypeError:
+            _type = type(_object.__getattribute__(_object, attr_str))
+        except AttributeError:
+            _type = "AttributeError happened"
+        except OSError:
+            _type = "OSError happened"
+        _type = Str.substring(str(_type), "<class ", ">", safe=True)
+        print(f"{Str.rightpad(attr_str, longest_name, '—')}>{_type}")
 
 
 def multiple(*types):
