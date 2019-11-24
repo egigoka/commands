1f6c68ce38cffd6db5f1484db500ad475ab9a006
Egor Egorov <egigoka@gmail.com>
2019-11-24 11:41:49 +0300

    added KiB, MiB, GiB in const

--- commands/const9.py
+++ commands/const9.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """Internal module with constants
 """
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 
 # pylint: disable=invalid-name, anomalous-backslash-in-string
 newline = '\n'  # string with LF newline
@@ -10,3 +10,6 @@ ruble = u"\u20bd"  # string with ₽ symbol
 backslash = "\ "[:1]  # string with backslash
 newline2 = "\r\n"  # string with CRLF newline
 rtl = "\u202e"  # string with Right-To-Left character https://resources.infosecinstitute.com/spoof-using-right-to-left-override-rtlo-technique-2/
+KiB = 1024
+MiB = KiB*1024
+GiB = MiB*1024
