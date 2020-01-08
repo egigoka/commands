684c569732e935405bed59268b80b0f997e5d1a2
Egor Egorov <egigoka@gmail.com>
2020-01-08 10:12:30 +0300

    added Archive and Unarchive

--- commands/_version.py
+++ commands/_version.py
@@ -1,7 +1,7 @@
 #! python3
 # -*- coding: utf-8 -*-
 """Here version stay and thoughts for updates"""
-__version__ = "9.0.0-alpha475"
+__version__ = "9.0.0-alpha476"
 
 # TODO for 9.0.0 release:
 #    !done! OS class vars not strings, but booleans
