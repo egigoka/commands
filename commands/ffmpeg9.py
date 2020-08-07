5d4c79f35f9ecc32015743ae4edbe37eeaf52c57
Egor Egorov <egigoka@gmail.com>
2020-08-07 11:55:48 +0500

    "fixed bug with quotes FFMpeg.add_subtitles_to_mkv"

--- commands/ffmpeg9.py
+++ commands/ffmpeg9.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """Internal module to work with ffmpeg
 """
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 
 class FFMpeg:
@@ -16,18 +16,16 @@ class FFMpeg:
         subs_meta_str = ''
         subs_meta_id = ID()
         for name, lang in Dict.iterable(subs):
-           subs_names_str += f'-i "{name}" '
+           subs_names_str += f'-i {name} '
            subs_meta_str += f'-metadata:s:s:{subs_meta_id.get()} language={lang} '
 
         maps_str = ''
         for i in range(len(subs)+1):
             maps_str += f'-map {i} '
 
-        command = f'ffmpeg -i "{input_file}" {subs_names_str.strip()} {maps_str.strip()} -c copy {subs_meta_str.strip()} "{output_file}"'
-
-        print(command)
+        command = f'ffmpeg -i {input_file} {subs_names_str.strip()} {maps_str.strip()} -c copy {subs_meta_str.strip()} {output_file}'
 
         working = 'ffmpeg -i "Doctor.Who.s05e01.mkv" -i "Doctor.Who.s05e01en.srt" -i "Doctor.Who.s05e01.srt" -map 0 ' \
                   '-map 1 -map 2 -c copy -metadata:s:s:0 language=eng -metadata:s:s:1 language=rus "output.mkv"'
 
-        Console.get_output(command, print_std=True, pureshell=True)
+        Console.get_output(command, print_std=True)
