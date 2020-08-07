74625a79e2f039bcd8665144d257a640e99741fe
Egor Egorov <egigoka@gmail.com>
2020-08-07 14:10:12 +0500

    "added support for files with spaces FFMpeg.add_subtitles_to_mkv"

--- commands/ffmpeg9.py
+++ commands/ffmpeg9.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """Internal module to work with ffmpeg
 """
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 
 
 class FFMpeg:
@@ -12,20 +12,17 @@ class FFMpeg:
         from .dict9 import Dict
         from .id9 import ID
 
-        subs_names_str = ''
-        subs_meta_str = ''
+        subs_names_list = []
+        subs_meta_list = []
         subs_meta_id = ID()
         for name, lang in Dict.iterable(subs):
-           subs_names_str += f'-i {name} '
-           subs_meta_str += f'-metadata:s:s:{subs_meta_id.get()} language={lang} '
+            subs_names_list += ['-i', '{name}']
+            subs_meta_list += [f'-metadata:s:s:{subs_meta_id.get()}', f'language={lang}']
 
-        maps_str = ''
+        maps_list = []
         for i in range(len(subs)+1):
-            maps_str += f'-map {i} '
+            maps_list += ['-map', i]
 
-        command = f'ffmpeg -i {input_file} {subs_names_str.strip()} {maps_str.strip()} -c copy {subs_meta_str.strip()} {output_file}'
+        commands = ["ffmpeg", "-i", input_file] + subs_names_list + maps_list + ["-c", "copy"] + subs_meta_list + [output_file]
 
-        working = 'ffmpeg -i "Doctor.Who.s05e01.mkv" -i "Doctor.Who.s05e01en.srt" -i "Doctor.Who.s05e01.srt" -map 0 ' \
-                  '-map 1 -map 2 -c copy -metadata:s:s:0 language=eng -metadata:s:s:1 language=rus "output.mkv"'
-
-        Console.get_output(command, print_std=True)
+        Console.get_output(commands, print_std=True)
