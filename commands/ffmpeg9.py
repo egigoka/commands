78d66a4cf2c6b08c14dc4ff225f7986642a1f0bb
Egor Egorov <egigoka@gmail.com>
2020-08-19 09:15:47 +0500

    "added some more debug logs to FFMpeg.add_something_to_mkv"

--- commands/ffmpeg9.py
+++ commands/ffmpeg9.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """Internal module to work with ffmpeg
 """
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 
 
 class FFMpeg:
@@ -13,7 +13,7 @@ class FFMpeg:
 
     @staticmethod
     def add_something_to_mkv(input_file: str, additional_files: dict,
-                             output_file: str, metadata_type: str, existed_langs: list = ()):
+                             output_file: str, metadata_type: str, existed_langs: list = (), debug: bool = False):
         import os
         from .console9 import Console
         from .dict9 import Dict
@@ -37,24 +37,33 @@ class FFMpeg:
         commands = ["ffmpeg", "-y", "-i", input_file] + names_list + maps_list + ["-c", "copy"] + meta_list + [
             output_file]
 
-        Console.get_output(commands, print_std=True)
+        if debug:
+            from .print9 import Print
+            Print.colored(f"{commands=}", "red")
+            Print.colored('" "'.join(commands), "magenta")
+
+        Console.get_output(commands, print_std=True, debug=debug)
 
 
     @classmethod
-    def add_subtitles_to_mkv(cls, input_file: str, subs: dict, output_file: str, existed_langs: list = ()):
+    def add_subtitles_to_mkv(cls, input_file: str, subs: dict, output_file: str, existed_langs: list = (),
+                             debug: bool = False):
         cls.add_something_to_mkv(input_file=input_file,
                                  additional_files=subs,
                                  output_file=output_file,
                                  existed_langs=existed_langs,
-                                 metadata_type=FFMpeg.MetadataTypes.subtitles)
+                                 metadata_type=FFMpeg.MetadataTypes.subtitles,
+                                 debug=debug)
 
     @classmethod
-    def add_audio_to_mkv(cls, input_file: str, audio: dict, output_file: str, existed_langs: list = ()):
+    def add_audio_to_mkv(cls, input_file: str, audio: dict, output_file: str, existed_langs: list = (),
+                         debug: bool = False):
         cls.add_something_to_mkv(input_file=input_file,
                                  additional_files=audio,
                                  output_file=output_file,
                                  existed_langs=existed_langs,
-                                 metadata_type=FFMpeg.MetadataTypes.audio)
+                                 metadata_type=FFMpeg.MetadataTypes.audio,
+                                 debug=debug)
 
     @staticmethod
     def convert_avi_to_mkv(input_file, output_file, verbose=False):
