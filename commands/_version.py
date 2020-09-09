f7d58fa1f94c728823e29cc14728599c6cf9fb45
Egor Egorov <egigoka@gmail.com>
2020-09-09 14:30:20 +0500

    "make FFMpeg python3.7- compatible"

--- commands/_version.py
+++ commands/_version.py
@@ -1,7 +1,7 @@
 #! python3
 # -*- coding: utf-8 -*-
 """Here version stay and thoughts for updates"""
-__version__ = "0.9.581-alpha"
+__version__ = "0.9.582-alpha"
 
 # TODO for 9.0.0 release:
 #    !finished! OS class vars not strings, but booleans
