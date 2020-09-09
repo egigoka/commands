0db2a7f2538ef0b91ec722d3d103c0d022fa625a
Egor Egorov <egigoka@gmail.com>
2020-09-09 14:24:54 +0500

    "make JsonDict python3.7- compatible"

--- commands/git9.py
+++ commands/git9.py
@@ -3,7 +3,7 @@
 """Internal module to simplify work with git
 """
 # pylint: disable=unused-wildcard-import, wildcard-import
-__version__ = "2.1.0"
+__version__ = "2.2.0"
 
 
 def get_name_of_repo():
@@ -21,16 +21,16 @@ class Git:
     """Class to simplify work with git, wrapper for cli git
     """
     @classmethod
-    def add(cls, what):
+    def add(cls, what, verbose=False):
         """Adds files to next commit
         <br>`param what` string, adding files
         <br>`return` None
         """
         from .console9 import Console
-        Console.get_output("git", "add", what)
+        Console.get_output("git", "add", what, print_std=verbose)
 
     @classmethod
-    def commit(cls, message=None):
+    def commit(cls, message=None, verbose=False):
         """Commit all added changes
         <br>`param message` string, message of commit
         <br>`return` None
@@ -42,10 +42,10 @@ class Git:
         if message:
             commands.append("-m")
             commands.append(Bash.argument_escape(message))
-        Console.get_output(commands)
+        Console.get_output(commands, print_std=verbose)
 
     @classmethod
-    def push(cls, path, upstream=False):
+    def push(cls, path, upstream=False, verbose=False):
         """Push commits to 'path' repo
         <br>`param path` string, path of repo
         <br>`param upstream` boolean, if True, adding argument '-u' to git
@@ -57,18 +57,18 @@ class Git:
         if upstream:
             commands.append("-u")
         commands.append(path)
-        Console.get_output(commands)
+        Console.get_output(commands, print_std=verbose)
 
     @classmethod
-    def update(cls, message, path="https://github.com/egigoka/" + get_name_of_repo() + ".git"):
+    def update(cls, message, path="https://github.com/egigoka/" + get_name_of_repo() + ".git", verbose=False):
         """Automatization to mine git upload
         <br>`param message` string, commit message
         <br>`param path` string, path to repo
         <br>`return` None
         """
-        cls.add(".")
-        cls.commit(message)
-        cls.push(path, upstream=True)
+        cls.add(".", verbose=verbose)
+        cls.commit(message, verbose=verbose)
+        cls.push(path, upstream=True, verbose=verbose)
 
 
 
