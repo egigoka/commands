ca17662b0f37e3a89a8c3df4c42a1198982ec8f0
egigoka <egigoka@gmail.com>
2021-04-15 15:20:48 +0500

    "added Obj.cast_to"

--- commands/_version.py
+++ commands/_version.py
@@ -1,7 +1,7 @@
 #! python3
 # -*- coding: utf-8 -*-
 """Here version stay and thoughts for updates"""
-__version__ = "0.9.646-alpha"
+__version__ = "0.9.647-alpha"
 
 # TODO for 9.0.0 release:
 #    !finished! OS class vars not strings, but booleans
