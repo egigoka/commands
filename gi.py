b81b4da94c7fde01b70de6c8a2d07f09b01d823c
Egorov Egor <egigoka@gmail.com>
2023-02-10 15:36:06 +0600

    "updated gi -py --no-version-change"

--- gi.py
+++ gi.py
@@ -8,7 +8,7 @@ import os
 from commands import *
 from commands.git9 import Git
 
-__version__ = "1.3.6"
+__version__ = "1.4.0"
 
 
 def safe_run(command):
@@ -26,24 +26,25 @@ def safe_run(command):
 
 
 # CHANGING VERSION
-version_prefix = '__version__ = "'
-version_suffix = '"'
-version_path = Path.combine(Path.working(), "commands", "_version.py")  # path to version file
-version_text = File.read(version_path)  # text from version file
-__version__ = Str.substring(version_text, version_prefix, version_suffix)  # get version from plane text
-last_int = Str.get_integers(__version__)[-1]  # get only integer of alpha version
-if last_int < 0:
-    last_int = -last_int
-old_version = version_prefix + __version__ + version_suffix  # old string, that currently in file
+if "--no-version-change" not in OS.args:
+	version_prefix = '__version__ = "'
+	version_suffix = '"'
+	version_path = Path.combine(Path.working(), "commands", "_version.py")  # path to version file
+	version_text = File.read(version_path)  # text from version file
+	__version__ = Str.substring(version_text, version_prefix, version_suffix)  # get version from plane text
+	last_int = Str.get_integers(__version__)[-1]  # get only integer of alpha version
+	if last_int < 0:
+	    last_int = -last_int
+	old_version = version_prefix + __version__ + version_suffix  # old string, that currently in file
 
-new_version = version_prefix + Str.rreplace(__version__, last_int, last_int+1, 1) + version_suffix  # new string to replace old version
-new_version_text = version_text.replace(old_version, new_version)  # replacing
-if OS.windows:  # for not annoying git message about mess with LF and CRLF
-    new_version_text = new_version_text.replace(newline, newline2)
-File.write(version_path, new_version_text, mode="w")  # write result to file
+	new_version = version_prefix + Str.rreplace(__version__, last_int, last_int+1, 1) + version_suffix  # new string to replace old version
+	new_version_text = version_text.replace(old_version, new_version)  # replacing
+	if OS.windows:  # for not annoying git message about mess with LF and CRLF
+	    new_version_text = new_version_text.replace(newline, newline2)
+	File.write(version_path, new_version_text, mode="w")  # write result to file
 
-new_version_string = new_version.lstrip(version_prefix).rstrip(version_suffix)
-Print.colored("uploadin", new_version_string, "grey", "on_white")  # print to notice difference
+	new_version_string = new_version.lstrip(version_prefix).rstrip(version_suffix)
+	Print.colored("uploadin", new_version_string, "grey", "on_white")  # print to notice difference
 # CHANGING VERSION END
 
 ARGUMENTS = list(sys.argv)
